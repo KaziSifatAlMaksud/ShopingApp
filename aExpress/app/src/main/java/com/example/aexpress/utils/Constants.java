1fbb2e904f49a1077edd189e99969f788f550b44
Kazi Sifat Al Makusd <65750595+KaziSifatAlMaksud@users.noreply.github.com>
2023-05-08 11:01:54 +0600

    first commit

---
+++ aExpress/app/src/main/java/com/example/aexpress/utils/Constants.java
@@ -0,0 +1,25 @@
+package com.example.aexpress.utils;
+
+public class Constants {
+    public static String API_BASE_URL = "https://tutorials.mianasad.com/ecommerce";
+    public static String GET_CATEGORIES_URL = API_BASE_URL + "/services/listCategory";
+ //   public static String GET_PRODUCTS_URL = "/services/listProduct";
+  public static String GET_PRODUCTS_URL = "https://lgorithmbd.com/php_rest_app/api/products/read.php";
+
+    public static String GET_OFFERS_URL = "https://lgorithmbd.com/php_rest_app/api/featurednews/read.php";
+    public static String GET_PRODUCT_DETAILS_URL = API_BASE_URL + "/services/getProductDetails?id=";
+    public static String POST_ORDER_URL = API_BASE_URL + "/services/submitProductOrder";
+    public static String PAYMENT_URL = API_BASE_URL + "/services/paymentPage?code=";
+    public static String PRODUCTS_IMAGE_URL = "http://lgorithmbd.com/app/product/";
+
+   // public static String PRODUCTS_IMAGE_URL = API_BASE_URL + "/uploads/product/";
+
+
+    //public static String NEWS_IMAGE_URL = API_BASE_URL + "/uploads/news/";
+    public static String NEWS_IMAGE_URL = "http://lgorithmbd.com/app/hero/";
+
+
+
+    public static String CATEGORIES_IMAGE_URL = API_BASE_URL + "/uploads/category/";
+  // public static String PRODUCTS_IMAGE_URL = API_BASE_URL + "/uploads/product/";
+}
